9fcc745f63156e4b9750b5017e72f20ba5f6f8ba
amilcar-cibertec <I202331874@cibertec.edu.pe>
2026-07-06 23:30:06 -0500

    Fix: base-href para subcarpeta de GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1155499636"
+    serviceWorkerVersion: "757360751"
   }
 });
